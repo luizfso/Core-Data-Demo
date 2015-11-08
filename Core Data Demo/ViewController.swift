f01d681f1afdfecd5c8fb65917be128d225e4b5b
Luiz Fernando Santiago <luizfso@gmail.com>
2015-11-08 11:01:28 -0200

    My Class for Core Data
    My Class for Core Data

--- Core Data Demo/ViewController.swift
+++ Core Data Demo/ViewController.swift
@@ -7,12 +7,82 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        /*
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context)
+        
+        
+        newUser.setValue("Ralphie", forKey: "username")
+        
+        newUser.setValue("aaaa", forKey: "password")
+        
+        do {
+            
+            try context.save()
+        
+        
+        }catch{
+        
+            print("There is a problem!")
+            
+        }
+
+        */
+        
+        
+        let request = NSFetchRequest(entityName: "Users")
+        
+            //request.predicate = NSPredicate(format: "username = %@", "Ralphie")
+        
+            request.returnsObjectsAsFaults = false
+        
+        
+        do{
+            let results = try context.executeFetchRequest(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                    
+                    //context.deleteObject(result)
+                    
+                    //result.setValue("Ralphie", forKey: "username")
+                    
+                    do{
+                    try context.save()
+                    } catch{
+                    }
+                    if let username = result.valueForKey("username") as? String{
+                    
+                        print(username)
+                    }
+                    //print(result.valueForKey("username")!)
+                    //print(result.valueForKey("password")!)
+                }
+            
+            
+            }
+           
+            
+            
+        }catch{
+            
+            print("Fecth Failed")
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
